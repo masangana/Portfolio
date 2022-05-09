70328f7b6476a723b9c3e826849613dbb76cc821
masangana <alexmasangana@gmail.com>
2022-05-09 21:56:15 +0300

    Add js & css
    --Add js function for show and hide menu
    --fix html issues in the nav bar
    --fix css properties for the nav bar

--- js/index.js
+++ js/index.js
@@ -5,5 +5,16 @@ const menuButton = document.getElementById('menu-button');
 
 
 function togglerMenu(){
-    menuElement.style.opacity = 0;
+    if (menuButton.classList.contains('open-menu')) {
+        menuElement.style.opacity = 1;
+        menuButton.classList.remove('open-menu');
+        menuButton.classList.add('close-menu')
+        console.log('on vient de ouvir')
+    } else if(menuButton.classList.contains('close-menu')) {
+        menuElement.style.opacity = 0;
+        menuButton.classList.remove('close-menu');
+        menuButton.classList.add('open-menu')
+        console.log('on vient de fermer')
+    }
+    
 }

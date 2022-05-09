ddfec638079aa2983b9c22c8ef5e071974745b3d
masangana <alexmasangana@gmail.com>
2022-05-09 16:52:25 +0300

    prepare dynamic
    -- Add Html & css ligne for mobile menu
    -- Add js file for
    -- Add items

---
+++ js/index.js
@@ -0,0 +1,9 @@
+
+const menuElement = document.getElementById('navigation-elements');
+const menuButton = document.getElementById('menu-button');
+
+
+
+function togglerMenu(){
+    menuElement.style.opacity = 0;
+}

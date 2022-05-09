18eb6b1869052de8b27346a6dbc1cfa0510fc729
masangana <alexmasangana@gmail.com>
2022-05-09 22:45:58 +0300

    fix js details
    -- removing scrolling

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 
 const menuElement = document.getElementById('navigation-elements');
 const menuButton = document.getElementById('menu-button');
+var page = document.getElementsByTagName('body')[0];
 
 
 
@@ -8,13 +9,15 @@ function togglerMenu(){
     if (menuButton.classList.contains('open-menu')) {
         menuElement.style.opacity = 1;
         menuButton.classList.remove('open-menu');
-        menuButton.classList.add('close-menu')
-        console.log('on vient de ouvir')
+        menuButton.classList.add('close-menu');
+        page.classList.add('noscroll');
     } else if(menuButton.classList.contains('close-menu')) {
         menuElement.style.opacity = 0;
         menuButton.classList.remove('close-menu');
-        menuButton.classList.add('open-menu')
-        console.log('on vient de fermer')
+        menuButton.classList.add('open-menu');
+        page.classList.remove('noscroll');
     }
     
 }
+
+//const navItems = document.

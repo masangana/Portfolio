cf52e90d517b8f8f8e717bf0c85f9779579360de
masangana <alexmasangana@gmail.com>
2022-05-10 00:52:08 +0300

    Fix html & css errors
    -- fixing css for the mobile menu
    -- removing html useless element

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 
 const menuElement = document.getElementById('navigation-elements');
 const menuButton = document.getElementById('menu-button');
+const logo =document.getElementById('logo');
 var page = document.getElementsByTagName('body')[0];
 
 
@@ -10,14 +11,22 @@ function togglerMenu(){
         menuElement.style.opacity = 1;
         menuButton.classList.remove('open-menu');
         menuButton.classList.add('close-menu');
+        logo.classList.add('text-logo-hidden');
         page.classList.add('noscroll');
     } else if(menuButton.classList.contains('close-menu')) {
         menuElement.style.opacity = 0;
         menuButton.classList.remove('close-menu');
         menuButton.classList.add('open-menu');
+        logo.classList.remove('text-logo-hidden');
         page.classList.remove('noscroll');
     }
     
 }
 
-//const navItems = document.
+const navItems = document.querySelectorAll('.mobile-nav-list');
+
+navItems.forEach(
+    (element)=> {
+        element.addEventListener('click', togglerMenu);
+      }
+)

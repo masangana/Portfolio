115908816e9273cdde5f91afb727d20b143bd0d2
rebel216 <neerajbhardwajpkt@gmail.com>
2022-05-12 03:16:01 +0530

    Add error Message

--- js/index.js
+++ js/index.js
@@ -1,122 +1,114 @@
+const menuElement = document.getElementById("navigation-elements");
+const menuButton = document.getElementById("menu-button");
+const logo = document.getElementById("logo");
+var page = document.getElementsByTagName("body")[0];
 
-const menuElement = document.getElementById('navigation-elements');
-const menuButton = document.getElementById('menu-button');
-const logo =document.getElementById('logo');
-var page = document.getElementsByTagName('body')[0];
-
-
-
-
-function togglerMenu(){
-    if (menuButton.classList.contains('open-menu')) {
-        menuElement.classList.remove('hide-menu');
-        menuElement.classList.add('show-menu');
-        menuButton.classList.remove('open-menu');
-        menuButton.classList.add('close-menu');
-        logo.classList.add('text-logo-hidden');
-        page.classList.add('noscroll');
-    } else if(menuButton.classList.contains('close-menu')) {
-        menuElement.classList.remove('show-menu');
-        menuElement.classList.add('hide-menu');
-        menuButton.classList.remove('close-menu');
-        menuButton.classList.add('open-menu');
-        logo.classList.remove('text-logo-hidden');
-        page.classList.remove('noscroll');
-    }
-    
+function togglerMenu() {
+  if (menuButton.classList.contains("open-menu")) {
+    menuElement.classList.remove("hide-menu");
+    menuElement.classList.add("show-menu");
+    menuButton.classList.remove("open-menu");
+    menuButton.classList.add("close-menu");
+    logo.classList.add("text-logo-hidden");
+    page.classList.add("noscroll");
+  } else if (menuButton.classList.contains("close-menu")) {
+    menuElement.classList.remove("show-menu");
+    menuElement.classList.add("hide-menu");
+    menuButton.classList.remove("close-menu");
+    menuButton.classList.add("open-menu");
+    logo.classList.remove("text-logo-hidden");
+    page.classList.remove("noscroll");
+  }
 }
 
-const navItems = document.querySelectorAll('.mobile-nav-list');
+const navItems = document.querySelectorAll(".mobile-nav-list");
 
-navItems.forEach(
-    (element)=> {
-        element.addEventListener('click', togglerMenu);
-      }
-)
+navItems.forEach((element) => {
+  element.addEventListener("click", togglerMenu);
+});
 
 /* Popup window */
 
 const modalData = [
-    {
-      id : 1,
-      main: true,
-      name: 'Portfolio',
-      languages: ['CSS', 'HTML', 'JavaScript'],
-      description:
-        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto atque possimus, repellendus recusandae dicta obcaecati minima expedita vero quidem et dolore impedit.',
-      img: './ressources/demo_portfolio.png',
-      link: 'https://akshithareddy1899.github.io/Microverse-Portfolio/',
-      source: 'https://github.com/AkshithaReddy1899/Microverse-Portfolio',
-    },
-    {
-      id : 2,
-      main: false,
-      name: 'Awa Nde',
-      languages: ['HTML', 'Bootstrap','Ruby'],
-      description:
-        'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusamus recusandae illum velit amet consectetur omnis aperiam aut. Incidunt quaerat ',
-      img: './ressources/Portfolioshow.png',
-    },
-    {
-      id : 3,
-      main: false,
-      name: 'Guilgald',
-      languages: ['HTML', 'JavaScript', 'Ruby'],
-      description:
-        'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusamus recusandae illum velit amet consectetur omnis aperiam aut. Incidunt quaerat ',
-      img: './ressources/Portfolioshow.png',
-    },
-    {
-      id : 4,
-      main: false,
-      name: 'Demo Project',
-      languages: ['HTML', 'Bootstrap', 'Ruby'],
-      description:
-        'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusamus recusandae illum velit amet consectetur omnis aperiam aut. Incidunt quaerat consequatur magnam! Rerum sunt quos doloribus illo!',
-      img: './ressources/Portfolioshow.png',
-    },
-    {
-      id : 5,
-      main: false,
-      name: 'New Demo',
-      languages: ['HTML', 'Bootstrap', 'Ruby'],
-      description:
-        'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusamus recusandae illum velit amet consectetur omnis aperiam aut. Incidunt quaerat ',
-      img: './ressources/Portfolioshow.png',
-    },
-    {
-      id : 6,
-      main: false,
-      name: 'Weather App',
-      languages: ['HTML', 'Bootstrap', 'Ruby'],
-      description:
-        'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusamus recusandae illum velit amet consectetur omnis aperiam aut. Incidunt quaerat ',
-      img: './ressources/Portfolioshow.png',
-    },
-    {
-      id : 7,
-      main: false,
-      name: 'Landing Page',
-      languages: ['HTML', 'Bootstrap', 'Ruby'],
-      description:
-        'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusamus recusandae illum velit amet consectetur omnis aperiam aut. Incidunt quaerat ',
-      img: './ressources/Portfolioshow.png',
-    },
-  ];
-  
-  /**
-   * code for popup window
-   */
-  
-   modalData.forEach((project, index) =>{
-    let lang ="";
-    let identity ="";
-    if(project.main){
-    
-      project.languages.forEach((element) => {
-        lang += `<li class="language-list-item">${element}</li>`;
-      });
-      document.querySelector('#main-grid').innerHTML = `
+  {
+    id: 1,
+    main: true,
+    name: "Portfolio",
+    languages: ["CSS", "HTML", "JavaScript"],
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto atque possimus, repellendus recusandae dicta obcaecati minima expedita vero quidem et dolore impedit.",
+    img: "./ressources/demo_portfolio.png",
+    link: "https://akshithareddy1899.github.io/Microverse-Portfolio/",
+    source: "https://github.com/AkshithaReddy1899/Microverse-Portfolio",
+  },
+  {
+    id: 2,
+    main: false,
+    name: "Awa Nde",
+    languages: ["HTML", "Bootstrap", "Ruby"],
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusamus recusandae illum velit amet consectetur omnis aperiam aut. Incidunt quaerat ",
+    img: "./ressources/Portfolioshow.png",
+  },
+  {
+    id: 3,
+    main: false,
+    name: "Guilgald",
+    languages: ["HTML", "JavaScript", "Ruby"],
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusamus recusandae illum velit amet consectetur omnis aperiam aut. Incidunt quaerat ",
+    img: "./ressources/Portfolioshow.png",
+  },
+  {
+    id: 4,
+    main: false,
+    name: "Demo Project",
+    languages: ["HTML", "Bootstrap", "Ruby"],
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusamus recusandae illum velit amet consectetur omnis aperiam aut. Incidunt quaerat consequatur magnam! Rerum sunt quos doloribus illo!",
+    img: "./ressources/Portfolioshow.png",
+  },
+  {
+    id: 5,
+    main: false,
+    name: "New Demo",
+    languages: ["HTML", "Bootstrap", "Ruby"],
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusamus recusandae illum velit amet consectetur omnis aperiam aut. Incidunt quaerat ",
+    img: "./ressources/Portfolioshow.png",
+  },
+  {
+    id: 6,
+    main: false,
+    name: "Weather App",
+    languages: ["HTML", "Bootstrap", "Ruby"],
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusamus recusandae illum velit amet consectetur omnis aperiam aut. Incidunt quaerat ",
+    img: "./ressources/Portfolioshow.png",
+  },
+  {
+    id: 7,
+    main: false,
+    name: "Landing Page",
+    languages: ["HTML", "Bootstrap", "Ruby"],
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusamus recusandae illum velit amet consectetur omnis aperiam aut. Incidunt quaerat ",
+    img: "./ressources/Portfolioshow.png",
+  },
+];
+
+/**
+ * code for popup window
+ */
+
+modalData.forEach((project, index) => {
+  let lang = "";
+  let identity = "";
+  if (project.main) {
+    project.languages.forEach((element) => {
+      lang += `<li class="language-list-item">${element}</li>`;
+    });
+    document.querySelector("#main-grid").innerHTML = `
           <div class="works-section-project">
             <img src="${project.img}" alt="${project.name}">
           </div>
@@ -132,11 +124,11 @@ const modalData = [
             </ul>
             <button data-index="${index}" type="button" class="project-button button display-modal popup-btn">See Project</button>
           </div>`;
-    }else{
-      project.languages.forEach((element) => {
-        lang += `<li class="language-list-item works-grid-one-project-language-list">${element}</li>`;
-      });
-      document.querySelector('#secondary-grid').innerHTML += `
+  } else {
+    project.languages.forEach((element) => {
+      lang += `<li class="language-list-item works-grid-one-project-language-list">${element}</li>`;
+    });
+    document.querySelector("#secondary-grid").innerHTML += `
         <div class="works-grid-one-project-container first-grid style="background-image: url('${project.img}')">
           <div class="grid-project1 works-grid-project">
             <h3 class="works-grid-one-project-header">${project.name}</h3>
@@ -150,53 +142,52 @@ const modalData = [
           </div>
           <button type="button" data-index="${index}" class="grid-project-button display-modal button">See Project</button>
         </div>`;
-    }
-    
-  });
-  
-  const openPopup = (index) => {
-    let lang = '';
-    modalData[index].languages.forEach((element) => {
-      lang += `<li class="language-list-item">${element}</li>`;
-    });
-    document.querySelector('.modal-container').style.display = 'block';
-    document.querySelector('#popuptitle').textContent = modalData[index].name;
-    document.querySelector('#modallanguages').innerHTML = lang;
-    document.querySelector('#modal-description').textContent = modalData[index].description;
-    document
-      .querySelector('#modal-img')
-      .setAttribute('src', modalData[index].img);
-      document
-      .querySelector('#modal-img')
-      .setAttribute('alt', modalData[index].name);
-  };
-  
-  const modalDisplayBtn = document.querySelectorAll('.display-modal');
-  
-  modalDisplayBtn.forEach((element) => {
-    element.addEventListener('click', () => {
-      openPopup(element.getAttribute('data-index'));
-    });
-  });
-  
-  document.querySelector('.modal-close-btn').addEventListener('click', () => {
-    document.querySelector('.modal-container').style.display = 'none';
-    page.classList.remove('noscroll');
-  });
-  
+  }
+});
 
-  /* FORM VALIDATIONS */
+const openPopup = (index) => {
+  let lang = "";
+  modalData[index].languages.forEach((element) => {
+    lang += `<li class="language-list-item">${element}</li>`;
+  });
+  document.querySelector(".modal-container").style.display = "block";
+  document.querySelector("#popuptitle").textContent = modalData[index].name;
+  document.querySelector("#modallanguages").innerHTML = lang;
+  document.querySelector("#modal-description").textContent =
+    modalData[index].description;
+  document
+    .querySelector("#modal-img")
+    .setAttribute("src", modalData[index].img);
+  document
+    .querySelector("#modal-img")
+    .setAttribute("alt", modalData[index].name);
+};
 
-const submit = document.getElementById('submit');
-const emailError = document.getElementById('error');
+const modalDisplayBtn = document.querySelectorAll(".display-modal");
 
-submit.addEventListener('click', (event) => {
-  const email = document.getElementById('email').value;
+modalDisplayBtn.forEach((element) => {
+  element.addEventListener("click", () => {
+    openPopup(element.getAttribute("data-index"));
+  });
+});
+
+document.querySelector(".modal-close-btn").addEventListener("click", () => {
+  document.querySelector(".modal-container").style.display = "none";
+  page.classList.remove("noscroll");
+});
+
+/* FORM VALIDATIONS */
+
+const submit = document.getElementById("submit");
+const emailError = document.getElementById("error");
+
+submit.addEventListener("click", (event) => {
+  const email = document.getElementById("email").value;
   const emailValidation = /[A-Z]/.test(email);
 
   if (emailValidation) {
-    emailError.textContent = 'Please enter email in lowercase';
+    emailError.textContent =
+      "Please enter a valid email , ensure that it is in the lowercase";
     event.preventDefault();
   }
 });
-  
